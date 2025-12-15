de95bfa436508531e3ffedf7a65e16a599df7c7a
padmaja-koti <padmajakoti961@gmail.com>
2025-12-15 19:38:08 +0530

    Resolved merge conflict

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,14 +1,8 @@
 class HelloWorld:
     def main(self):
         print("Hello World from Git and GitHub")
-<<<<<<< HEAD
         print("Hello World – edited locally in VS Code")
-
-
-=======
-        print("Hello World – edited directly on GitHub")
         print("This line was added on GitHub")
->>>>>>> f01045e59b15591bb4f513093887a899a3cd4041
 
 if __name__ == "__main__":
     HelloWorld().main()
