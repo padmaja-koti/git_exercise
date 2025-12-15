f01045e59b15591bb4f513093887a899a3cd4041
padmaja-koti <166643542+padmaja-koti@users.noreply.github.com>
2025-12-15 19:06:46 +0530

    Update hello_world.py

--- src/hello_world.py
+++ src/hello_world.py
@@ -2,7 +2,7 @@ class HelloWorld:
     def main(self):
         print("Hello World from Git and GitHub")
         print("Hello World – edited directly on GitHub")
-
+        print("This line was added on GitHub")
 
 if __name__ == "__main__":
     HelloWorld().main()
